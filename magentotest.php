df207bf43b96e964c76297628401258361bc5793
Hugo Zonderland <info@hugozonderland.nl>
2013-05-06 13:13:36 +0200

    Geen idee wat, maar er is iets gewijzigd.

---
+++ magentotest.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: hugozonderland
+ * Date: 03-05-13
+ * Time: 21:05
+ * To change this template use File | Settings | File Templates.
+ */
+
+require_once('MagentoClientLib/MagentoClient.php');
+
+$apiUser = 'Hugo';
+$apiKey = 'Ka0yoiAOJhoifqap0oinhlkqfn0oe8vh0234gtQ965WGEWROIUHJWEROIGNRESD98OHL234TWP98YWERFGNLKAERGO87HKJN234TPHJKZWGRHLI';
+$host = 'http://magento.presteren.nu/api/soap/?wsdl';
+
+$client = new MagentoClient($apiUser, $apiKey, $host);
+$client->connect();
+$data = (array)$client->getOrders();
+
+print "<pre>";
+print_r($data);
+print "</pre>";
